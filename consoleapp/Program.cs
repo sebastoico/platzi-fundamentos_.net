4595c3d5a8b93765be5fdd1a45b10f6633677e03
Sebastián Jaramillo <108204749+sebastoico@users.noreply.github.com>
2023-11-02 17:31:45 +0000

    "Descargando un paquete con NuGet"

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("Por favor ingrese un nombre:");
+﻿using Humanizer;
+using System;
+
+Console.WriteLine("Por favor ingrese un nombre:");
 var nombre = Console.ReadLine();
 Console.WriteLine("Por favor ingrese su cargo:");
 var cargo = Console.ReadLine();
 Console.WriteLine("Por favor ingrese su edad:");
-var edad = Console.ReadLine();
+var edad = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Hola, mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años.");
+Console.WriteLine($"Hola, mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años.");
