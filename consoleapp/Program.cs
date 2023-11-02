32a9941cf33c58eec71db9fdf4d3e74264d707df
Sebastián Jaramillo <108204749+sebastoico@users.noreply.github.com>
2023-11-02 16:44:22 +0000

    "Compilación y ejecución"

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Por favor ingrese un nombre:");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo:");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad:");
+var edad = Console.ReadLine();
+
+Console.WriteLine($"Hola, mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años.");
